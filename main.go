22f7d78b4aa62f75a6f958f73e02bae794b2e7a9
Mahlet Mulugeta <161637299+mahletm2020@users.noreply.github.com>
2024-07-04 12:06:15 -0700

    bycrypt aded

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-   "github.com/golang-jwt/jwt/v5"
+   //"log"
+	// "github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 	"github.com/gin-gonic/gin"
 )
 type User struct {
@@ -13,40 +15,41 @@ type User struct {
 	Password string
  }
  func addUser(ctx *gin.Context) {
-		body := User{}
-		data, err := ctx.GetRawData()
-		if err != nil {
-			 ctx.AbortWithStatusJSON(400, "User is not defined")
-			 return
-		}
-		err = json.Unmarshal(data, &body)
-		if err != nil {
-			 ctx.AbortWithStatusJSON(400, "Bad Input")
-			 return
-		}
-	 //use Exec whenever we want to insert update or delete
-	//Doing Exec(query) will not use a prepared statement, so lesser TCP calls to the SQL server
-		_, err = database.Db.Exec("insert into users(username,password) values ($1,$2)", body.Username, body.Password)
-		if err != nil {
-			 fmt.Println(err)
-			 ctx.AbortWithStatusJSON(400, "Couldn't create the new user.")
-		} else {
-			 ctx.JSON(http.StatusOK, "User is successfully created.")
-		}
- 
- }
-func main() {
-		// gin.SetMode(gin.ReleaseMode) //optional to not get warning
-		// route.SetTrustedProxies([]string{"192.168.1.2"}) //to trust only a specific value
-		route := gin.Default()
-		database.ConnectDatabase()
-		route.POST("/add", addUser)
-
-
-	err := route.Run(":8080")
+	body := User{}
+	data, err := ctx.GetRawData()
 	if err != nil {
-		 panic(err)
+		ctx.AbortWithStatusJSON(400, "User is not defined")
+		return
+	}
+	err = json.Unmarshal(data, &body)
+	if err != nil {
+		ctx.AbortWithStatusJSON(400, "Bad Input")
+		return
 	}
 
+	//  Generate a bcrypt hash from the password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(body.Password), bcrypt.DefaultCost)
+	if err != nil {
+		ctx.AbortWithStatusJSON(500, "Error hashing password")
+		return
+	}
+	fmt.Println("Received Username:", body.Username)
+	fmt.Println("Received Password:", body.Password)
+	//  Store the hashed password in the database
+	_, err = database.Db.Exec("insert into users(username,password) values ($1,$2)", body.Username, string(hashedPassword))
+
+	if err != nil {
+		fmt.Println(err)
+		ctx.AbortWithStatusJSON(400, "Couldn't create the new user.")
+	} else {
+		ctx.JSON(http.StatusOK, "User is successfully created.")
+	}
 }
-//u stoped at  installing the jwt packae and importing it next time do the password part using bycrypt and then implemen it using this bae doc( Mukesh Murugan)​.
+
+
+func main() {
+	database.ConnectDatabase()
+	router := gin.Default()
+	router.POST("/users", addUser)
+	router.Run("localhost:8080")
+}
