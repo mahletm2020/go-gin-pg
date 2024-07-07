548db9a011863df679c76cedf114b662ee0950d3
Mahlet Mulugeta <161637299+mahletm2020@users.noreply.github.com>
2024-07-07 02:35:18 -0700

    fixed

--- main.go
+++ main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"GO-PG/database"
+	"GO-PG/auth"
 	"encoding/json"
 	"fmt"
 	"net/http"
-   //"log"
-	// "github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 	"github.com/gin-gonic/gin"
 )
+
 type User struct {
-	Username string
-	Password string
- }
- func addUser(ctx *gin.Context) {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func addUser(ctx *gin.Context) {
 	body := User{}
 	data, err := ctx.GetRawData()
 	if err != nil {
@@ -27,7 +28,7 @@ type User struct {
 		return
 	}
 
-	//  Generate a bcrypt hash from the password
+	// Generate a bcrypt hash from the password
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(body.Password), bcrypt.DefaultCost)
 	if err != nil {
 		ctx.AbortWithStatusJSON(500, "Error hashing password")
@@ -35,7 +36,7 @@ type User struct {
 	}
 	fmt.Println("Received Username:", body.Username)
 	fmt.Println("Received Password:", body.Password)
-	//  Store the hashed password in the database
+	// Store the hashed password in the database
 	_, err = database.Db.Exec("insert into users(username,password) values ($1,$2)", body.Username, string(hashedPassword))
 
 	if err != nil {
@@ -46,10 +47,73 @@ type User struct {
 	}
 }
 
+func loginUser(ctx *gin.Context) {
+	body := User{}
+	err := ctx.BindJSON(&body)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, "Invalid request")
+		return
+	}
+
+	var storedUser User
+	err = database.Db.QueryRow("SELECT username, password FROM users WHERE username=$1", body.Username).Scan(&storedUser.Username, &storedUser.Password)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, "Invalid username or password")
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(storedUser.Password), []byte(body.Password))
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, "Invalid username or password")
+		return
+	}
+
+	token, err := auth.GenerateJWT(storedUser.Username)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, "Error generating token")
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"token": token,
+	})
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, "Authorization header missing")
+			return
+		}
+
+		tokenString := authHeader[len("Bearer "):]
+		claims, err := auth.ValidateToken(tokenString)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, "Invalid token")
+			return
+		}
+
+		ctx.Set("username", claims.Username)
+		ctx.Next()
+	}
+}
 
 func main() {
 	database.ConnectDatabase()
 	router := gin.Default()
+
 	router.POST("/users", addUser)
+	router.POST("/login", loginUser)
+
+	protected := router.Group("/protected")
+	protected.Use(AuthMiddleware())
+	{
+		protected.GET("/data", func(ctx *gin.Context) {
+			username := ctx.GetString("username")
+			ctx.JSON(http.StatusOK, gin.H{"message": "Hello " + username})
+		})
+	}
+
 	router.Run("localhost:8080")
 }
